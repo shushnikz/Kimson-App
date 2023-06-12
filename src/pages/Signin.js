f79c9e2fdf3ceae7345c4442f5c1357f4c1366ce
shushnikz <sushmithadevadiga997@gmail.com>
2023-06-12 19:33:41 +0530

    added signup and login page

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import DefaultLayout from '../components/DefaultLayout'
+import { Button, Col, Form, Input, Row } from 'antd'
 
 const Signin = () => {
     return (
-        <div>
-            <h1>Sign in</h1>
-            <Link to="/signup">Click here to sign up</Link>
+        <div className="register">
+            <Row justify='center' className="flex align-tems-center">
+                <Col lg={10} sm={24} className="bs p-5 mt-5 register-form">
+                    <h3>Login Page</h3>
+                    <hr />
+                    <Form layout="vertical">
+                        <Form.Item label="username" name="username" rules={[{ required: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="password" name="password" rules={[{ required: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Button className="mb-3 btn btn-dark" htmlType='submit'>Sign in</Button>
+                        <br></br>
+                        <Link to="/signup">Don't have an account? Click here to Register</Link>
+                    </Form>
+                </Col>
+            </Row>
         </div>
     )
 }
